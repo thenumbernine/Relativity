a601eed4ece3e6d8436c71ded1b116cc52eca595
root <christopher.e.moore@gmail.com>
2013-08-27 03:44:17 +0000

    organized the tests a bit.  started on the whole camel-case classes-are-capitol rule, but I have a ways to go before that is consistent everywhere.

--- relativity.cpp
+++ relativity.cpp
@@ -5,13 +5,18 @@
 
 using namespace std;
 
-namespace test {
+namespace Test {
 
 typedef double real;
 enum { dim = 1 };
 typedef ::vector<real,dim> vector;
 typedef ::ADMFormalism<real,dim> ADMFormalism;
 typedef ADMFormalism::deref_type deref_type;
+
+typedef ADMFormalism::tensor_l tensor_l;
+typedef ADMFormalism::tensor_u tensor_u;
+typedef ADMFormalism::tensor_sl tensor_sl;
+typedef ADMFormalism::tensor_su tensor_su;
 
 //universal constants
 const real speedOfLightInMPerS = 299792458.;
@@ -28,154 +33,164 @@ const real sunRadiusInM = 6.955e+8;
 const real sunVolumeInM3 = 4. / 3. * M_PI * sunRadiusInM * sunRadiusInM * sunRadiusInM;
 const real sunDensityInM_2 = sunMassInKg * metersPerKg / sunVolumeInM3;
 
-void sun () {
-	real maxDist = 2. * sunRadiusInM;
-	vector min(-maxDist);
-	vector max(maxDist);
-	deref_type res(10);
-
-	cout << "constructing sim..." << endl;
-	ADMFormalism sim(min, max, res);
-
-	//provide initial conditions
-
-	cout << "providing initial conditions..." << endl;
-	vector center = (max + min) * .5;
-	for (ADMFormalism::GridIter iter = sim.readCells->begin(); iter != sim.readCells->end(); ++iter) {
-		ADMFormalism::Cell &cell = *iter;
-		vector v = sim.coordForIndex(iter.index) - center;
-		real r = vector::length(v);
-		real sunMassOver2Radius = sunMassInM / (2. * r);
-		real oneMinusSunMassOver2Radius = 1. - sunMassOver2Radius;
-		real onePlusSunMassOver2Radius = 1. + sunMassOver2Radius;
-		real onePlusSunMassOver2RadiusSq = onePlusSunMassOver2Radius * onePlusSunMassOver2Radius;
-		cell.alpha = oneMinusSunMassOver2Radius / onePlusSunMassOver2Radius; 
-		for (int i = 0; i < dim; ++i) {
-			cell.gamma_ll(i,i) = onePlusSunMassOver2RadiusSq * onePlusSunMassOver2RadiusSq;
-		}
-		if (r <= sunRadiusInM) {
-			cell.rho = sim.dx.volume() * sunDensityInM_2;
-		}
+struct Base {
+	real maxDist;
+	vector min, max, center;
+	deref_type res;
+	ADMFormalism *sim;
+			
+	Base(real maxDist_, int res_) 
+	: maxDist(maxDist_), min(-maxDist_), max(maxDist_), res(res_), sim(NULL) {
+		center = (max + min) * .5;
 	}
 
-	//update
-	//cout << "iterating..." << endl;
-	//const real dt = .01;
-	//sim.update(dt);
+	virtual const string filename() const = 0;
 
-	cout << "done!" << endl;
-}
+	virtual void init() {
+		cout << "constructing sim..." << endl;
+		sim = new ADMFormalism(min, max, res);
+	}
+	
+	virtual void run() {
+		ofstream f(filename().c_str());
+		sim->outputHeaders(f);
+		sim->outputLine(f);
+
+		//update
+		//cout << "iterating..." << endl;
+		//const real dt = .01;
+		//sim->update(dt);
+
+		cout << "done!" << endl;
+	}
+};
+
+struct Sun : public Base {
+	Sun() : Base(2. * sunRadiusInM, 10) {}
+
+	virtual const string filename() const { return "sun.txt"; }
+
+	virtual void init() {
+		Base::init();
+		
+		//provide initial conditions
+		cout << "providing initial conditions..." << endl;
+		for (ADMFormalism::GridIter iter = sim->readCells->begin(); iter != sim->readCells->end(); ++iter) {
+			ADMFormalism::Cell &cell = *iter;
+			vector v = sim->coordForIndex(iter.index) - center;
+			real r = vector::length(v);
+			real sunMassOver2Radius = sunMassInM / (2. * r);
+			real oneMinusSunMassOver2Radius = 1. - sunMassOver2Radius;
+			real onePlusSunMassOver2Radius = 1. + sunMassOver2Radius;
+			real onePlusSunMassOver2RadiusSq = onePlusSunMassOver2Radius * onePlusSunMassOver2Radius;
+			cell.alpha = oneMinusSunMassOver2Radius / onePlusSunMassOver2Radius; 
+			for (int i = 0; i < dim; ++i) {
+				cell.gamma_ll(i,i) = onePlusSunMassOver2RadiusSq * onePlusSunMassOver2RadiusSq;
+			}
+			if (r <= sunRadiusInM) {
+				cell.rho = sim->dx.volume() * sunDensityInM_2;
+			}
+		}
+	}
+};
 
 /*
 See the Kerr-Schild section of the scratch paper in the README
 */
-void black_hole() {
-	real maxDist = 2. * sunRadiusInM;
-	vector min(-maxDist);
-	vector max(maxDist);
-	deref_type res(10);
+struct BlackHole : public Base {
+	BlackHole() : Base(2. * sunRadiusInM, 10) {}
 
-	cout << "constructing sim..." << endl;
-	ADMFormalism sim(min, max, res);
-	
-	const real blackHoleMassInKg = 4. * sunMassInM;	//2.7 solar masses is upper bound for neutron stars
-	const real M = blackHoleMassInKg * metersPerKg;	//black hole mass in meters
-	
-	const real Q = 0.;		//total charge
-	const real J = 0.;		//total angular momentum
-	const real a = J / M;	//angular momentum density
-	
-	typedef ADMFormalism::tensor_l tensor_l;
-	typedef ADMFormalism::tensor_u tensor_u;
-	typedef ADMFormalism::tensor_sl tensor_sl;
-	typedef ADMFormalism::tensor_su tensor_su;
+	virtual const string filename() const { return "black_hole.txt"; }
 
-	tensor_sl eta;
-	for (int i = 0; i < dim; ++i) {
-		eta(i,i) = 1.;
-	}
+	virtual void init() {
+		Base::init();
 
-	//provide initial conditions
-	
-	cout << "providing initial conditions..." << endl;
-	vector center = (max + min) * .5;
-	for (ADMFormalism::GridIter iter = sim.readCells->begin(); iter != sim.readCells->end(); ++iter) {
-		ADMFormalism::Cell &cell = *iter;
-		vector v = sim.coordForIndex(iter.index) - center;
-		real r = vector::length(v); 
-		real x = v(0);
-		real y = v(1);
-		real z = v(2);
-		real H = (r * M - Q * Q / 2.) / (r * r + a * a * z * z / (r * r));
+		const real blackHoleMassInKg = 4. * sunMassInM;	//2.7 solar masses is upper bound for neutron stars
+		const real M = blackHoleMassInKg * metersPerKg;	//black hole mass in meters
 		
-		tensor_l l_l;
-		l_l(0) = (r * x + a * y) / (r * r + a * a);
-		l_l(1) = (r * y - a * x) / (r * r + a * a);
-		l_l(2) = z / r;
+		const real Q = 0.;		//total charge
+		const real J = 0.;		//total angular momentum
+		const real a = J / M;	//angular momentum density
 		
-		tensor_sl &gamma_ll = cell.gamma_ll;
+		tensor_sl eta;
 		for (int i = 0; i < dim; ++i) {
-			for (int j = 0; j <= i; ++j) {
-				gamma_ll(i,j) = eta(i,j) + (2. - eta(i,j)) * H * l_l(i) * l_l(j);
-			}
-		}
-		
-		tensor_su &gamma_uu = cell.gamma_uu;
-		gamma_uu = invert(gamma_ll);
-
-		tensor_u l_u;
-		for (int i = 0; i < dim; ++i) {
-			l_u(i) = 0.;
-			for (int j = 0; j < dim; ++j) {
-				l_u(i) += gamma_uu(i,j) * l_l(j);
-			}
+			eta(i,i) = 1.;
 		}
 
-		tensor_u &beta_u = cell.beta_u;
-		beta_u = l_u * H;
-
-		real betaNorm = 0.;
-		for (int i = 0; i < dim; ++i) {
-			for (int j = 0; j < dim; ++j) {
-				betaNorm += gamma_ll(i,j) * beta_u(i) * beta_u(j);
-			}
-		}
-
-		real &alpha = cell.alpha;
-		alpha = sqrt(betaNorm - 1. - 2. * H);
-
-		tensor_sl &K_ll = cell.K_ll;
-		for (int i = 0; i < dim; ++i) {
-			for (int j = 0; j <= i; ++j) {
-				K_ll(i,j) = 2. * H * a / r * (eta(i,j) - (2. + H) * l_l(i) * l_l(j));
-			}
-		}
+		//provide initial conditions
 		
-		real sunMassOver2Radius = sunMassInM / (2. * sunRadiusInM);
-		real oneMinusSunMassOver2Radius = 1. - sunMassOver2Radius;
-		real onePlusSunMassOver2Radius = 1. + sunMassOver2Radius;
-		real onePlusSunMassOver2RadiusSq = onePlusSunMassOver2Radius * onePlusSunMassOver2Radius;
-		cell.alpha = oneMinusSunMassOver2Radius / onePlusSunMassOver2Radius; 
-		for (int i = 0; i < dim; ++i) {
-			cell.gamma_ll(i,i) = onePlusSunMassOver2RadiusSq * onePlusSunMassOver2RadiusSq;
+		cout << "providing initial conditions..." << endl;
+		vector center = (max + min) * .5;
+		for (ADMFormalism::GridIter iter = sim->readCells->begin(); iter != sim->readCells->end(); ++iter) {
+			ADMFormalism::Cell &cell = *iter;
+			vector v = sim->coordForIndex(iter.index) - center;
+			real r = vector::length(v); 
+			real x = v(0);
+			real y = v(1);
+			real z = v(2);
+			real H = (r * M - Q * Q / 2.) / (r * r + a * a * z * z / (r * r));
+			
+			tensor_l l_l;
+			l_l(0) = (r * x + a * y) / (r * r + a * a);
+			l_l(1) = (r * y - a * x) / (r * r + a * a);
+			l_l(2) = z / r;
+			
+			tensor_sl &gamma_ll = cell.gamma_ll;
+			for (int i = 0; i < dim; ++i) {
+				for (int j = 0; j <= i; ++j) {
+					gamma_ll(i,j) = eta(i,j) + (2. - eta(i,j)) * H * l_l(i) * l_l(j);
+				}
+			}
+			
+			tensor_su &gamma_uu = cell.gamma_uu;
+			gamma_uu = invert(gamma_ll);
+
+			tensor_u l_u;
+			for (int i = 0; i < dim; ++i) {
+				l_u(i) = 0.;
+				for (int j = 0; j < dim; ++j) {
+					l_u(i) += gamma_uu(i,j) * l_l(j);
+				}
+			}
+
+			tensor_u &beta_u = cell.beta_u;
+			beta_u = l_u * H;
+
+			real betaNorm = 0.;
+			for (int i = 0; i < dim; ++i) {
+				for (int j = 0; j < dim; ++j) {
+					betaNorm += gamma_ll(i,j) * beta_u(i) * beta_u(j);
+				}
+			}
+
+			real &alpha = cell.alpha;
+			alpha = sqrt(betaNorm - 1. - 2. * H);
+
+			tensor_sl &K_ll = cell.K_ll;
+			for (int i = 0; i < dim; ++i) {
+				for (int j = 0; j <= i; ++j) {
+					K_ll(i,j) = 2. * H * a / r * (eta(i,j) - (2. + H) * l_l(i) * l_l(j));
+				}
+			}
+			
+			real sunMassOver2Radius = sunMassInM / (2. * sunRadiusInM);
+			real oneMinusSunMassOver2Radius = 1. - sunMassOver2Radius;
+			real onePlusSunMassOver2Radius = 1. + sunMassOver2Radius;
+			real onePlusSunMassOver2RadiusSq = onePlusSunMassOver2Radius * onePlusSunMassOver2Radius;
+			cell.alpha = oneMinusSunMassOver2Radius / onePlusSunMassOver2Radius; 
+			for (int i = 0; i < dim; ++i) {
+				cell.gamma_ll(i,i) = onePlusSunMassOver2RadiusSq * onePlusSunMassOver2RadiusSq;
+			}
 		}
 	}
-
-	ofstream f("black_hole.txt");
-	sim.outputHeaders(f);
-	sim.outputLine(f);
-
-	//update
-	//cout << "iterating..." << endl;
-	//const real dt = .01;
-	//sim.update(dt);
-
-	cout << "done!" << endl;
-}
+};
 
 }
 
 int main() {
-	test::black_hole();
+	typedef ::Test::BlackHole Test;
+	Test test;
+	test.init();
+	test.run();
 }
 
